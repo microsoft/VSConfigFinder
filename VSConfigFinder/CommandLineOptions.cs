79bbadaa0ef00c9dc55aad0232b81dc7a95a4689
Skylar <rorelei@gmail.com>
2023-02-10 10:36:29 -0800

    Address comments

--- VSConfigFinder/CommandLineOptions.cs
+++ VSConfigFinder/CommandLineOptions.cs
@@ -11,7 +11,7 @@
 
         /// <inheritdoc/>
         [Option("createfile", Required = false, HelpText = "Set the folder path to output the consolidated .vsconfig.\n" +
-            "If the argument (--createfile) is not passed at all, the program will output the cli arguments that can be fed into the Visual Studio Installer setup arguments. (e.g. --add --add Microsoft.VisualStudio.Component.Roslyn.Compiler --add Microsoft.Net.Component.4.8.SDK\n" +
+            "If the argument (--createfile) is not passed at all, the program will output the cli arguments that can be fed into the Visual Studio Installer setup arguments. (e.g. --add Microsoft.VisualStudio.Component.Roslyn.Compiler Microsoft.Net.Component.4.8.SDK)\n" +
             "If the argument is passed without a following folder path, the program will create and output the final .vsconfig into the current directory.\n" +
             "If the argument is passed with a following folder path, the program will create and output the final .vsconfig into the specified folder path that is followed by --createFile.")]
         public string? CreateFile { get; set; }
